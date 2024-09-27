5a3570a26c61664c4fd6093fada564b5cd78db30
AnkurNahar <ankurnahar.an@gmail.com>
2024-09-27 16:59:26 -0700

    props and destructuring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+//destructuring
+const [firstCity, second] = [
+  "Tokyo",
+  "Tahoe City",
+  "Bend"
+];
+
+console.log(firstCity);
+console.log(second);
+
+//props
+function App({ library }) { //can also do props
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from {library}</h1>  {/* props.library */}
     </div>
   );
 }

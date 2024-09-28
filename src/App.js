b52a334df248952eaf6d1670b11c02f4b437c576
AnkurNahar <ankurnahar.an@gmail.com>
2024-09-27 18:41:01 -0700

    hooks: useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
-//destructuring
-const [firstCity, second] = [
-  "Tokyo",
-  "Tahoe City",
-  "Bend"
-];
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] =
+    useState("tired");
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
 
-console.log(firstCity);
-console.log(second);
-
-//props
-function App({ library }) { //can also do props
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>  {/* props.library */}
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>
+        Sad
+      </button>
+      <button
+        onClick={() => setEmotion("excited")}
+      >
+        Excited
+      </button>
+      <h2>
+        Current secondary emotion is {secondary}.
+      </h2>
+      <button
+        onClick={() => setSecondary("grateful")}
+      >
+        Grateful
+      </button>
     </div>
   );
 }
